18b4f546e21389b8045f25e5394f83528592b4bf
deepurrobotics <zhan2838@purdue.edu>
2018-12-14 15:35:23 -0500

    create script for obtaining image and command

---
+++ scripts/get_img_cmd_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import numpy as np
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+
+def rgb_cb(data):
+  global cv_image
+  bridge = CvBridge()
+  cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+  cv2.imshow("Image window", cv_image)
+  cv2.waitKey(3)
+
+def cmdvel_cb(data):
+  global cmd_vel
+  cmd_vel = data.data
+  rospy.logdebug("cmd_vel: {}".format(cmd_vel))
+
+if __name__ == '__main__':
+  rospy.loginfo("Initializing node... ")
+  rospy.init_node("collect_data")
+  rospy.Subscriber("cmd_vel", Twist, cmdvel_cb)
+  rospy.Subscriber("color/image_raw", Image, rgb_cb)
+  command_pool = []
+  image_pool = []
+  rate = rospy.Rate(10) # 10 Hz
+  while not rospy.is_shutdown():
+    command_pool.append(cmd_vel)
+    image_pool.append(cv_image)
+    rate.sleep()
+  
